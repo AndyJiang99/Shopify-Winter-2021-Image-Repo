452beeca8c0e713c309efd2a2c8b172487ceb04f
Andy Jiang <Andy.Jiang@ibm.com>
2020-09-07 17:59:00 -0400

    updated ports for heroku

--- app.py
+++ app.py
@@ -229,4 +229,5 @@ def deletePictures():
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     write_key()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
